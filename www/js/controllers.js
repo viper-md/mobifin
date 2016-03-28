81dbb9fe4fd891955b7db2e293447b06a8bb00f9
viper-md <12bce126@nirmauni.ac.in>
2016-03-28 16:36:10 +0530

    commit

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+      
+.controller('registerAccountCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+ 
